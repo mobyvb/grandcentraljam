b198534823e59effdaf8aa578c38a6691db6b16f
Maximillian von Briesen <mobyvb@gmail.com>
2015-06-17 19:42:30 -0400

    add some basic user routing

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,42 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user.js');
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  User.find({}, function(err, users) {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json(users);
+    }
+  });
+});
+
+router.post('/', function(req, res, next) {
+  var newUser = new User({
+    email: req.body.email,
+    username: req.body.username,
+    password: req.body.password
+  });
+
+  newUser.save(function(err) {
+    if (err) {
+      res.json(err);
+    } else {
+      res.redirect('/users/' + req.body.username);
+    }
+  });
+});
+
+router.get('/:username', function(req, res, next) {
+  console.log('getting ' + req.params.username);
+  User.findOne({username: req.params.username}, function(err, user) {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json(user);
+    }
+  });
 });
 
 module.exports = router;

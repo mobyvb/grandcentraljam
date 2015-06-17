67f739231b2d20981a5a8c3fd815b0cdc97c316c
Maximillian von Briesen <mobyvb@gmail.com>
2015-06-16 23:05:16 -0400

    layout some basic infrastructure

---
+++ mongoose/index.js
@@ -0,0 +1,7 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/gcj');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback() {
+  console.log('Connected to DB');
+});

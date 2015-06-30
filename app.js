be1964f92d17608bab6ec24861423391ab29e1e8
Maximillian von Briesen <mobyvb@gmail.com>
2015-06-30 19:10:39 -0400

    add browserify and do some other stuff

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var browserify = require('browserify-middleware');
 var db = require('./mongoose');
 
 var routes = require('./routes/index');
@@ -22,6 +23,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/components', express.static(path.join(__dirname, 'bower_components')));
+app.use('/js', browserify('./client/js'));
 
 app.use('/', routes);
 app.use('/users', users);
